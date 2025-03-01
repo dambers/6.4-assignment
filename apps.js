7bc49de2307cbf0f435570151857d283bed71beb
dambers <demetris.ambers@outlook.com>
2025-02-28 22:52:20 -0600

    Add initial HTML and JavaScript files for assignment 6.4

---
+++ apps.js
@@ -0,0 +1,23 @@
+// object and object methods
+
+const user = {
+    firstname: "Demetris",
+    lastname: "Ambers",
+    age: 34,
+    college: true, 
+    hobbies: ["comics, play pool, gym"],
+    fullname: function () {
+        document.write(this.firstname+ " " +this.lastname);
+    }
+};
+console.log(fullname);
+
+
+//object arrays methods
+let fruits = ["grapes", "bananas", "peaches"];
+
+fruits [0] = "strawberries";
+
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
